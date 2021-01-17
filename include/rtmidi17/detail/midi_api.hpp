813844a2566bd6decdc270555a53d8c05e628b64
Jean-Michaël Celerier <jeanmichael.celerier@gmail.com>
2021-01-17 23:54:38 +0100

    Implement a very basic input for raw alsa - does not work for complex messages yet

--- include/rtmidi17/detail/midi_api.hpp
+++ include/rtmidi17/detail/midi_api.hpp
@@ -1,6 +1,7 @@
 #pragma once
 #include <iostream>
 #include <rtmidi17/rtmidi17.hpp>
+#include <rtmidi17/detail/midi_queue.hpp>
 #include <string_view>
 
 namespace rtmidi
@@ -171,58 +172,6 @@ public:
 
     return inputData_.queue.pop(m);
   }
-
-  struct midi_queue
-  {
-    unsigned int front{};
-    unsigned int back{};
-    unsigned int ringSize{};
-    std::unique_ptr<message[]> ring{};
-
-    bool push(const message& msg)
-    {
-      auto [sz, _, b] = get_dimensions();
-
-      if (sz < ringSize - 1)
-      {
-        ring[b] = msg;
-        back = (back + 1) % ringSize;
-        return true;
-      }
-
-      return false;
-    }
-    bool pop(message& msg)
-    {
-      auto [sz, f, _] = get_dimensions();
-
-      if (sz == 0)
-      {
-        return false;
-      }
-
-      // Copy queued message to the vector pointer argument and then "pop" it.
-      using namespace std;
-      swap(msg, ring[f]);
-
-      // Update front
-      front = (front + 1) % ringSize;
-      return true;
-    }
-
-    struct dimensions
-    {
-      unsigned int size, front, back;
-    };
-    dimensions get_dimensions() const
-    {
-      // Access back/front members exactly once and make stack copies for
-      // size calculation ==> completely unneccessary
-      // https://godbolt.org/g/HPu9LA
-
-      return {(back >= front) ? back - front : ringSize - front + back, front, back};
-    }
-  };
 
   // The RtMidiInData structure is used to pass private class data to
   // the MIDI input handling function or thread.
