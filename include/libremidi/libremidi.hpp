dda581f229bcf31d2a705255c441dd6025b37d02
Jean-Michaël Celerier <jeanmichael.celerier@gmail.com>
2021-04-12 18:47:34 +0200

    Split API enum definition in a separate file

--- include/libremidi/libremidi.hpp
+++ include/libremidi/libremidi.hpp
@@ -58,6 +58,7 @@
 */
 
 #include <libremidi/message.hpp>
+#include <libremidi/api.hpp>
 
 #include <algorithm>
 #include <cassert>
@@ -169,30 +170,6 @@ struct LIBREMIDI_EXPORT thread_error final : public midi_exception
     a warning) is reported.
  */
 using midi_error_callback = std::function<void(midi_error type, std::string_view errorText)>;
-
-//! MIDI API specifier arguments.
-enum class API
-{
-  UNSPECIFIED, /*!< Search for a working compiled API. */
-  MACOSX_CORE, /*!< Macintosh OS-X Core Midi API. */
-  LINUX_ALSA, /*!< The Advanced Linux Sound Architecture API. */
-  LINUX_ALSA_SEQ = LINUX_ALSA,
-  LINUX_ALSA_RAW, /*!< Raw ALSA API. */
-  UNIX_JACK, /*!< The JACK Low-Latency MIDI Server API. */
-  WINDOWS_MM, /*!< The Microsoft Multimedia MIDI API. */
-  WINDOWS_UWP, /*!< The Microsoft WinRT MIDI API. */
-  EMSCRIPTEN_WEBMIDI, /*!< Web MIDI API through Emscripten */
-  DUMMY       /*!< A compilable but non-functional API. */
-};
-
-/**
- * \brief A static function to determine the available compiled MIDI APIs.
-
-  The values returned in the std::vector can be compared against
-  the enumerated list values.  Note that there can be more than one
-  API compiled for certain operating systems.
-*/
-std::vector<libremidi::API> available_apis() noexcept;
 
 //! A static function to determine the current version.
 std::string get_version() noexcept;
