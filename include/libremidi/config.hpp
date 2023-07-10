be99d756648f89c672c987f5c5d609d97a0afadf
Jean-Michaël Celerier <jeanmichael.celerier@gmail.com>
2023-07-10 11:34:23 -0400

    [code] Clang-format

--- include/libremidi/config.hpp
+++ include/libremidi/config.hpp
@@ -23,8 +23,9 @@
 #define LIBREMIDI_VERSION "3.0.0"
 
 #if defined(LIBREMIDI_USE_BOOST)
-  #if ! __has_include(<boost/container/small_vector.hpp>)
-    #error "Boost was used for building libremidi but is not found when using it. Add Boost to your include paths."
+  #if !__has_include(<boost/container/small_vector.hpp>)
+    #error \
+        "Boost was used for building libremidi but is not found when using it. Add Boost to your include paths."
   #endif
 
   #if defined(LIBREMIDI_NO_BOOST)
@@ -33,7 +34,7 @@
 #endif
 
 #if __has_include(<boost/container/small_vector.hpp>) && !defined(LIBREMIDI_NO_BOOST)
-#include <boost/container/small_vector.hpp>
+  #include <boost/container/small_vector.hpp>
 namespace libremidi
 {
 using midi_bytes = boost::container::small_vector<unsigned char, 4>;
