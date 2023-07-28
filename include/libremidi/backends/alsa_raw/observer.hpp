458a2dc9723a8e30b4869551ef4d3b73b4ba0818
Jean-Michaël Celerier <jeanmichael.celerier@gmail.com>
2023-07-28 19:43:17 -0400

    [core] Fix for no udev

--- include/libremidi/backends/alsa_raw/observer.hpp
+++ include/libremidi/backends/alsa_raw/observer.hpp
@@ -211,9 +211,9 @@ private:
 }
 #else
   #include <libremidi/backends/dummy.hpp>
-namespace libremidi
+namespace libremidi::alsa_raw
 {
-struct observer_alsa_raw : observer_dummy
+struct observer_impl : observer_dummy
 {
   explicit observer_alsa_raw(
       observer_configuration&& conf, alsa_raw_observer_configuration&& apiconf)
