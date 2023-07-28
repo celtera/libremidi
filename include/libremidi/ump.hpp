9c003990ec72a9842d9928a5541ba892358704b9
Jean-Michaël Celerier <jeanmichael.celerier@gmail.com>
2023-07-28 11:16:33 -0400

    [core] Fix constexpr call to non-constexpr

--- include/libremidi/ump.hpp
+++ include/libremidi/ump.hpp
@@ -8,13 +8,14 @@ namespace libremidi
 {
 struct ump
 {
-  alignas(4) uint32_t bytes[4];
+  alignas(4) uint32_t bytes[4] = {};
   int64_t timestamp{};
 
   constexpr operator std::span<const uint32_t>() const noexcept {
     return {bytes, size()}; }
 
   constexpr ump() noexcept = default;
+  constexpr ~ump() = default;
   constexpr ump(uint32_t b0) noexcept
       : bytes{b0}
   {
@@ -32,12 +33,25 @@ struct ump
   {
   }
 
-  constexpr std::size_t size() const noexcept {
-    auto res = cmidi2_ump_get_num_bytes(bytes[0]);
-    if(res == 0xFF)
-      return 0;
-    return res / 4;
+  constexpr std::size_t size() const noexcept
+  {
+    switch (((bytes[0] & 0xF0000000) >> 28) & 0xF)
+    {
+      case CMIDI2_MESSAGE_TYPE_UTILITY:
+      case CMIDI2_MESSAGE_TYPE_SYSTEM:
+      case CMIDI2_MESSAGE_TYPE_MIDI_1_CHANNEL:
+        return 1;
+      case CMIDI2_MESSAGE_TYPE_MIDI_2_CHANNEL:
+      case CMIDI2_MESSAGE_TYPE_SYSEX7:
+        return 2;
+      case CMIDI2_MESSAGE_TYPE_SYSEX8_MDS:
+        return 4;
+      default:
+        return 0;
+    }
   }
+
+  constexpr void clear() noexcept { bytes[0] = 0; }
 
   constexpr auto& operator[](int i) const noexcept { return bytes[i]; }
   constexpr auto& operator[](int i) noexcept { return bytes[i]; }
