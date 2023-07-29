f5e4fb35a57a97ef4f4a47ad928072eaef1b3c76
Jean-Michaël Celerier <jeanmichael.celerier@gmail.com>
2023-07-29 17:31:42 -0400

    [emscripten] Fix build

--- examples/emscripten_midiin.cpp
+++ examples/emscripten_midiin.cpp
@@ -1,6 +1,6 @@
 #include <libremidi/configurations.hpp>
 #include <libremidi/libremidi.hpp>
-
+#include "utils.hpp"
 #include <emscripten.h>
 
 #include <array>
@@ -13,31 +13,27 @@
  */
 int main(int argc, char**)
 {
-  std::vector<std::shared_ptr<libremidi::midi_in>> inputs;
-  std::vector<std::shared_ptr<libremidi::midi_out>> outputs;
+  std::vector<libremidi::midi_in> inputs;
+  std::vector<libremidi::midi_out> outputs;
 
   libremidi::observer_configuration callbacks{
       .input_added =
-          [&](const libremidi::port_information& id) {
+          [&](const libremidi::input_port& id) {
     std::cout << "MIDI Input connected: " << id.port_name << std::endl;
 
     auto conf = libremidi::input_configuration{
         .on_message = [](const libremidi::message& msg) { std::cout << msg << std::endl; }};
-    auto input
-        = std::make_shared<libremidi::midi_in>(conf, libremidi::emscripten_input_configuration{});
-
-    input->open_port(id);
-
-    inputs.push_back(input);
+    auto& input = inputs.emplace_back(conf, libremidi::emscripten_input_configuration{});
+    input.open_port(id);
       },
 
       .input_removed =
-          [&](const libremidi::port_information& id) {
+          [&](const libremidi::input_port& id) {
     std::cout << "MIDI Input removed: " << id.port_name << std::endl;
       },
 
       .output_added
-      = [&](const libremidi::port_information& id) {
+      = [&](const libremidi::output_port& id) {
     std::cout << "MIDI Output connected: " << id.port_name << std::endl;
 
     libremidi::midi_out output{};
@@ -46,11 +42,14 @@ int main(int argc, char**)
     output.send_message(0x80, 64, 100);
       },
 
-      .output_removed = [&](const libremidi::port_information& id) {
+      .output_removed = [&](const libremidi::output_port& id) {
         std::cout << "MIDI Output removed: " << id.port_name << std::endl;
       }};
 
-  libremidi::observer obs{libremidi::API::WEBMIDI, std::move(callbacks)};
+  libremidi::observer obs{
+      std::move(callbacks)
+    , libremidi::observer_configuration_for(libremidi::API::WEBMIDI)
+  };
 
   emscripten_set_main_loop([] {}, 60, 1);
 }
