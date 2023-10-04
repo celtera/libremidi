857b16ae550afc140adf761d1487cde302645ffe
Jean-Michaël Celerier <jeanmichael.celerier@gmail.com>
2023-10-04 18:23:54 -0400

    v4.2.1

--- include/libremidi/config.hpp
+++ include/libremidi/config.hpp
@@ -20,7 +20,7 @@
   #define LIBREMIDI_EXPORT
 #endif
 
-#define LIBREMIDI_VERSION "4.2.0"
+#define LIBREMIDI_VERSION "4.2.1"
 
 #if defined(LIBREMIDI_USE_BOOST)
   #if !__has_include(<boost/container/small_vector.hpp>)
