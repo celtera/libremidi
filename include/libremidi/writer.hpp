e9c18f3a3460d1ef9b3bd9de348cec87caa9d5d4
Jean-Michaël Celerier <jeanmichael.celerier@gmail.com>
2024-08-31 18:25:38 -0400

    [ci] Another missing include

--- include/libremidi/writer.hpp
+++ include/libremidi/writer.hpp
@@ -27,6 +27,7 @@ OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #include <libremidi/message.hpp>
 
 #include <cstdint>
+#include <iosfwd>
 #include <vector>
 
 namespace libremidi
