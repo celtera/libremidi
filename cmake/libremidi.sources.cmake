75a88370533125dcc3ca684e31e1c5090224d40b
Jean-Michaël Celerier <jeanmichael.celerier@gmail.com>
2025-03-17 19:23:24 +0100

    cmake: cleanup sources.cmake

--- cmake/libremidi.sources.cmake
+++ cmake/libremidi.sources.cmake
@@ -1,38 +1,46 @@
 target_sources(libremidi PRIVATE
-    include/libremidi/backends/alsa_seq/config.hpp
-    include/libremidi/backends/alsa_seq/helpers.hpp
-    include/libremidi/backends/alsa_seq/midi_in.hpp
-    include/libremidi/backends/alsa_seq/midi_out.hpp
-    include/libremidi/backends/alsa_seq/observer.hpp
-    include/libremidi/backends/alsa_seq/shared_handler.hpp
-
     include/libremidi/backends/alsa_raw/config.hpp
     include/libremidi/backends/alsa_raw/helpers.hpp
     include/libremidi/backends/alsa_raw/midi_in.hpp
     include/libremidi/backends/alsa_raw/midi_out.hpp
     include/libremidi/backends/alsa_raw/observer.hpp
+    include/libremidi/backends/alsa_raw.hpp
 
     include/libremidi/backends/alsa_raw_ump/config.hpp
     include/libremidi/backends/alsa_raw_ump/helpers.hpp
     include/libremidi/backends/alsa_raw_ump/midi_in.hpp
     include/libremidi/backends/alsa_raw_ump/midi_out.hpp
     include/libremidi/backends/alsa_raw_ump/observer.hpp
+    include/libremidi/backends/alsa_raw_ump.hpp
+
+    include/libremidi/backends/alsa_seq/config.hpp
+    include/libremidi/backends/alsa_seq/helpers.hpp
+    include/libremidi/backends/alsa_seq/midi_in.hpp
+    include/libremidi/backends/alsa_seq/midi_out.hpp
+    include/libremidi/backends/alsa_seq/observer.hpp
+    include/libremidi/backends/alsa_seq/shared_handler.hpp
+    include/libremidi/backends/alsa_seq.hpp
 
     include/libremidi/backends/alsa_seq_ump/config.hpp
     include/libremidi/backends/alsa_seq_ump/helpers.hpp
     include/libremidi/backends/alsa_seq_ump/midi_out.hpp
+    include/libremidi/backends/alsa_seq_ump.hpp
 
     include/libremidi/backends/coremidi/config.hpp
     include/libremidi/backends/coremidi/helpers.hpp
     include/libremidi/backends/coremidi/midi_in.hpp
     include/libremidi/backends/coremidi/midi_out.hpp
     include/libremidi/backends/coremidi/observer.hpp
+    include/libremidi/backends/coremidi.hpp
 
     include/libremidi/backends/coremidi_ump/config.hpp
     include/libremidi/backends/coremidi_ump/helpers.hpp
     include/libremidi/backends/coremidi_ump/midi_in.hpp
     include/libremidi/backends/coremidi_ump/midi_out.hpp
     include/libremidi/backends/coremidi_ump/observer.hpp
+    include/libremidi/backends/coremidi_ump.hpp
+
+    include/libremidi/backends/dummy.hpp
 
     include/libremidi/backends/emscripten/config.hpp
     include/libremidi/backends/emscripten/helpers.hpp
@@ -44,6 +52,7 @@ target_sources(libremidi PRIVATE
     include/libremidi/backends/emscripten/midi_out.hpp
     include/libremidi/backends/emscripten/observer.cpp
     include/libremidi/backends/emscripten/observer.hpp
+    include/libremidi/backends/emscripten.hpp
 
     include/libremidi/backends/jack/config.hpp
     include/libremidi/backends/jack/error_domain.hpp
@@ -52,11 +61,13 @@ target_sources(libremidi PRIVATE
     include/libremidi/backends/jack/midi_out.hpp
     include/libremidi/backends/jack/observer.hpp
     include/libremidi/backends/jack/shared_handler.hpp
+    include/libremidi/backends/jack.hpp
 
     include/libremidi/backends/jack_ump/config.hpp
     include/libremidi/backends/jack_ump/midi_in.hpp
     include/libremidi/backends/jack_ump/midi_out.hpp
     include/libremidi/backends/jack_ump/observer.hpp
+    include/libremidi/backends/jack_ump.hpp
 
     include/libremidi/backends/keyboard/config.hpp
     include/libremidi/backends/keyboard/midi_in.hpp
@@ -86,29 +97,20 @@ target_sources(libremidi PRIVATE
     include/libremidi/backends/winmidi/midi_in.hpp
     include/libremidi/backends/winmidi/midi_out.hpp
     include/libremidi/backends/winmidi/observer.hpp
+    include/libremidi/backends/winmidi.hpp
 
     include/libremidi/backends/winmm/config.hpp
     include/libremidi/backends/winmm/helpers.hpp
     include/libremidi/backends/winmm/midi_in.hpp
     include/libremidi/backends/winmm/midi_out.hpp
     include/libremidi/backends/winmm/observer.hpp
+    include/libremidi/backends/winmm.hpp
 
     include/libremidi/backends/winuwp/config.hpp
     include/libremidi/backends/winuwp/helpers.hpp
     include/libremidi/backends/winuwp/midi_in.hpp
     include/libremidi/backends/winuwp/midi_out.hpp
     include/libremidi/backends/winuwp/observer.hpp
-
-    include/libremidi/backends/alsa_raw.hpp
-    include/libremidi/backends/alsa_raw_ump.hpp
-    include/libremidi/backends/alsa_seq.hpp
-    include/libremidi/backends/alsa_seq_ump.hpp
-    include/libremidi/backends/coremidi.hpp
-    include/libremidi/backends/coremidi_ump.hpp
-    include/libremidi/backends/dummy.hpp
-    include/libremidi/backends/emscripten.hpp
-    include/libremidi/backends/jack.hpp
-    include/libremidi/backends/winmm.hpp
     include/libremidi/backends/winuwp.hpp
 
     include/libremidi/detail/memory.hpp
